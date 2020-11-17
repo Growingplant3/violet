050ee1af201b1d870d10119ea15ab9a7759322ab
下田　佳紀 <y.s@platinum.local>
2020-11-18 00:01:35 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(title: "Laravel", body: "PHPの人気フレームワークといえば、やっぱりLaravelですね！")
+Article.create(title: "Rails", body: "Rubyの人気フレームワークといえば、やっぱりRailsですね！")
